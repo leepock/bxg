54b27de9e5b6356d3aae6df193d7aeb35df98f6b
leepock <996896535@qq.com>
2017-09-12 13:19:51 +0800

    完成讲师查看功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,12 +1,37 @@
-define(['jquery','template'],function ($,template) {
+define(['jquery','template','bootstrap'],function ($,template) {
         $.ajax({
             type : 'get',
             url : '/api/teacher',
             dataType : 'json',
             success : function(data){
-                console.log(data)
                 var html = template('teacherInfoTpl',{list:data.result});
                 $('#teacherInfo').html(html)
+                //绑定查看讲师信息的单击事件
+                $('#teacherInfo').find('.precherInfo').click(function () {
+                    var tcId = $(this).closest('td').attr('data-id');
+
+                    $.ajax({
+                        type : 'get',
+                        url : '/api/teacher/view',
+                        data : {tc_id :tcId},
+                        dataType : 'json',
+                        success : function (data) {
+                            data.result.tc_hometown = data.result.tc_hometown.replace(/[|]/g,' ')
+                            var html = template('teacherModalInfoTpl',data.result);
+                            $('#teacherModalInfo').html(html);
+                            //显示弹窗
+                            $('#teachermodal').modal();
+                        }
+
+                    })
+
+                })
+
             }
         })
+
+
+
+
+
 })
